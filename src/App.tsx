b5247b07e6cbaa85f10fa255ef3db190ede4a138
kk3939 <kyo51310@gmail.com>
2021-11-03 16:19:20 +0900

    styleをセット

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import ContainerOne from "./component/ContainerOne";
 import ContainerTwo from "./component/ContainerTwo";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ContainerOne />
-      <ContainerTwo />
+      <Wrapper>
+        <ContainerOne />
+        <Spacer />
+        <ContainerTwo />
+      </Wrapper>
     </div>
   );
 };
+
+const Spacer = styled.div`
+  height: 100px;
+`;
+
+const Wrapper = styled.div`
+  margin: 50px auto;
+  text-align: center;
+`;
 
 export default App;
